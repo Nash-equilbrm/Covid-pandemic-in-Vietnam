e1e529763a7279626481175a328c2ef4e5fe1b9c
z0mb13-1705 <dohoainam17052001@gmail.com>
2022-01-12 17:17:42 +0700

    Initial commit

---
+++ scripts/get_vn_data.py
@@ -0,0 +1,27 @@
+# # cumulative-deaths-and-cases-covid-19
+# with open("./data/cumulative-deaths-and-cases-covid-19.csv","r") as fread:
+#     with open("./vn_data/vietnam-cumulative-deaths-and-cases-covid-19.csv","w") as fwrite:
+#         category = fread.readline()
+#         fwrite.write(category)
+#         lines = fread.readlines()
+#         for line in lines:
+#             category = line.split(',')
+#             if(category[0] == "Vietnam"):
+#                 fwrite.write(line)
+#     fread.close()
+
+# # share-people-fully-vaccinated-covid
+# with open("./data/share-people-fully-vaccinated-covid.csv","r") as fread:
+#     with open("./vn_data/vietnam-share-people-fully-vaccinated-covid.csv","w") as fwrite:
+#         category = fread.readline()
+#         fwrite.write(category)
+#         lines = fread.readlines()
+#         for line in lines:
+#             category = line.split(',')
+#             if(category[0] == "Vietnam"):
+#                 fwrite.write(line)
+#     fread.close()
+
+
+
+
